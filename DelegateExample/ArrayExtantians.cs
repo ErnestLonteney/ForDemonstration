ecfc7a998d1bbde22507688bfe39aae33cf1cb68
Volodymyr N <volodymyr@mail.com>
2023-08-18 20:26:13 +0200

    Added DelegateExample

---
+++ DelegateExample/ArrayExtantians.cs
@@ -0,0 +1,32 @@
+﻿namespace DelegateExample
+{
+    delegate bool FilterDelegate(int n);
+    static class ArrayExtantians
+    {
+        public static int[] Filter(this int[] array, FilterDelegate filterMethod)
+        {
+            int count = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (filterMethod.Invoke(array[i]))
+                    count++;
+            }
+
+            if (count == 0)
+                return Array.Empty<int>();
+
+            int[] temptArray = new int[count];
+
+            for (int i = 0, j = 0; i < array.Length; i++)
+            {
+                if (filterMethod.Invoke(array[i]))
+                {
+                    temptArray[j++] = array[i];
+                }
+            }
+
+            return temptArray;  
+        }
+    }
+}
---
+++ DelegateExample/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DelegateExample
+{
+    internal class Program
+    {
+        static void DisplayArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++) 
+                Console.Write($"{array[i]} ");              
+
+            Console.WriteLine();
+        }       
+
+        static void Main()
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var res1 = array.Filter(n => (n % 2) == 0);
+            var res2 = array.Filter(n => n > 5);
+
+            DisplayArray(res1); 
+            DisplayArray(res2); 
+        }
+    }
+}
