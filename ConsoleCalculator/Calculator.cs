e06c28f8490c623b5080503c2ff4564fce2d54f9
Volodymyr N <volodymyr@mail.com>
2023-08-28 22:01:35 +0200

    Added console calculator

---
+++ ConsoleCalculator/Calculator.cs
@@ -0,0 +1,23 @@
+﻿namespace CalculatorGeneric
+{
+    static class Calculator 
+    {
+        public static EventHandler DivideByZero = null;
+        public static double Add(double operand1, double operand2) => operand1 + operand2;
+
+        public static double Substruct(double operand1, double operand2) => operand1 - operand2;
+
+        public static double Multiply(double operand1, double operand2) => operand1 * operand2;
+
+        public static double? Divide(double operand1, double operand2)
+        {
+            if (operand2 == 0)
+            {
+                DivideByZero.Invoke(null, null);
+                return null;                   
+            }
+
+            return operand1 / operand2;
+        }      
+    }
+}
---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CalculatorGeneric
+{
+    internal class Program
+    {
+        static void DivideByZero(object sender, EventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Attempt divide by zero");
+            Console.ForegroundColor= ConsoleColor.White;
+        }
+
+        static void Main()
+        {
+            Calculator.DivideByZero += new EventHandler(DivideByZero);
+
+            double result = Calculator.Add(45, 47);
+            double? result2 = Calculator.Divide(47, 0);
+
+            Console.WriteLine(result);
+        }
+    }
+}
