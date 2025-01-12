576009166fa49fc311837cd9e23998acb30b8674
Volodymyr <Volodymyr@gmail.com>
2025-01-12 03:46:45 +0100

    used discard

--- TextReader/MainWindow.xaml.cs
+++ TextReader/MainWindow.xaml.cs
@@ -39,7 +39,7 @@ namespace TextReader
 
                     try
                     {
-                        Task.Run(async () =>
+                        _ = Task.Run(async () =>
                         {
                             while (!got)
                             {
